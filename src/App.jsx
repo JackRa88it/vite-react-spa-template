26b8d70dc4d55af6c736297430e3ccb12c1700f6
Jack Kent <jhkent88@gmail.com>
2024-04-16 19:13:48 -0700

    Add tab switching

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,40 @@ import './App.css'
 export default function App() {
   const [tab, setTab] = useState('home')
 
+  let currentTab
+  switch (tab) {
+    case 'home':
+      currentTab = <HomeTab />
+      break;
+    case 'about':
+      currentTab = <AboutTab />
+      break;
+    case 'contact':
+      currentTab = <ContactTab />
+      break;
+    default:
+      currentTab = <HomeTab />
+  }
+
   return (
     <>
       <Navbar 
         tab={tab}
         onTabClick={setTab}
       />
-      <HomeTab />
+      {currentTab}
     </>
   )
 }
 
 function Navbar({ tab, onTabClick }) {
   return (
-    <div className='navbar'>
-      Navbar
-    </div>
+    <nav className='navbar'>
+      <div className='logo' onClick={(e) => onTabClick('home')}>LOGO</div>
+      <div className='tab' onClick={(e) => onTabClick('home')}>Home</div>
+      <div className='tab' onClick={(e) => onTabClick('about')}>About</div>
+      <div className='tab' onClick={(e) => onTabClick('contact')}>Contact</div>
+    </nav>
   )
 }
 
@@ -27,6 +45,22 @@ function HomeTab() {
   return (
     <div className='home-tab'>
       Home tab
+    </div>
+  )
+}
+
+function AboutTab() {
+  return (
+    <div className='about-tab'>
+      About tab
+    </div>
+  )
+}
+
+function ContactTab() {
+  return (
+    <div className='contact-tab'>
+      Contact tab
     </div>
   )
 }
